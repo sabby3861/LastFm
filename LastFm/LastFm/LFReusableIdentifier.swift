2371b24ccfd53a4f8b2f585398eb4bf4dfd648ad
Sanjay Chauhan <sanjay.22.kumar@bt.com>
2021-07-12 17:54:27 +0100

    Project initialisation Created Storyboard helper class Created constant class

---
+++ LastFm/LastFm/LFReusableIdentifier.swift
@@ -0,0 +1,21 @@
+//
+//  LFReusableIdentifier.swift
+//  LastFm
+//
+//  Created by Sanjay Chauhan on 12/07/2021.
+//
+
+import Foundation
+/// Protocol to use resuse identifier
+protocol LFReusableIdentifier: class {
+    static var reuseIdentifier: String { get }
+}
+
+extension LFReusableIdentifier {
+    static var reuseIdentifier: String {
+        return "\(self)"
+    }
+}
+
+/// Constants to use
+let storyboardError = "Couldn't instantiate view controller with identifier"
